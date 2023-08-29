80da52282dd1bf5b5e87102dfada9ba05df555d1
QuantDeveloperUSA <82096803+QuantDeveloperUSA@users.noreply.github.com>
2023-08-28 22:31:41 -0400

    Adjusting the prompts

--- ai_assistant_for_attorneys.py
+++ ai_assistant_for_attorneys.py
@@ -1,21 +1,19 @@
 import streamlit as st
 from langchain.llms import OpenAI
 
-# for a parrot, we can use this character 🦜
-# for a chain, we can use this character 🔗
-# for a doctor, we can use this character 👨‍⚕️
-# for a PHD Professor, we can use this character 👨‍🏫
-# for an assistant, we can use this character 👨‍💼
-# for an Engineer, we can use this character 👨‍🔧
-# for A.I., we can use this character 🤖
-# for an Attorney, we can use this character 👨‍⚖️
-
-
 st.title('👨‍⚖️ Assistente I.A. para os melhores advogados do Brasil')
 
-openai_api_key = st.sidebar.text_input('Qual a palavra magica?')
-Context_for_assistant_Prompt = "A Dra Debora, uma advogada extremamente importante nas comarcas do Rio de Janeiro é PHD e incrivel em criar novas teorias. Ela perguntou a seu assistente: "
-Contextualize_the_Assistant_Answer = "O assistente, que é também advogado ilustrissimo, já foi inclusive juíz de direito, respondeu: "
+# openai_api_key will be read preferably from config.txt if the file exists, otherwise, it will be read from the enviroment variable API_TOKEN
+# Open the file config.txt and read the API_TOKEN from it
+try:
+  with open('config.txt') as f:
+    openai_api_key = f.readline()
+except:
+  openai_api_key = st.secrets["API_TOKEN"]
+#openai_api_key = st.sidebar.text_input('What is the Magic Word?')
+
+Context_for_assistant_Prompt = "O assistente Jarvis é uma Inteligencia Artificial criada pelo Renomado Engenheiro Roberto, um dos melhores engenheiros de Inteligencia Artificial do planeta terra e das galáxias. \nA Dra Debora, uma advogada extremamente importante nas comarcas do Rio de Janeiro no Brasil é muito criativa e super inteligente. Dra Debora perguntou a seu assistente Jarvis: "
+Contextualize_the_Assistant_Answer = "O assistente Jarvis, que é também advogado ilustrissimo, já foi inclusive juíz de direito e Desembargador, respondeu: "
 
 def generate_response(input_text):
   llm = OpenAI(temperature=0.7, openai_api_key=openai_api_key)
@@ -33,3 +31,5 @@ with st.form('my_form'):
 
 # to change the color of the submit button to green, we can use this code
 # submitted_to_assistant = st.form_submit_button('Ask the Assistant 👨‍💼', help='Click to submit the form')    
+
+#to run this app, use this command: streamlit run ai_assistant_for_attorneys.py
